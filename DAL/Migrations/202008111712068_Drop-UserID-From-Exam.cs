43df042dace41011744f6b80c97a34d5551f5863
SaarTuvi <saar.tuvi@gmail.com>
2020-08-11 20:53:58 +0300

    Added Migration To Try Remove User_ID From Exams

---
+++ DAL/Migrations/202008111712068_Drop-UserID-From-Exam.cs
@@ -0,0 +1,25 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DropUserIDFromExam : DbMigration
+    {
+        
+        public override void Up()
+        {
+            AddColumn("dbo.Exams", "ForMigration", c => c.String());
+            DropForeignKey("dbo.Exams", "User_Id", "dbo.Users");
+            DropIndex("dbo.Exams",new[] { "User_Id" });
+            DropColumn("dbo.Exams", "User_Id");
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Exams", "ForMigration");
+            AddColumn("dbo.Exams", "User_Id", c => c.String(maxLength: 128));
+            AddForeignKey("dbo.Exams", "User_Id", "dbo.Users", "Id");
+            CreateIndex("dbo.Exams", "User_Id");
+        }
+    }
+}
